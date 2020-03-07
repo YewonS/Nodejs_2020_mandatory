cdfea94072e7e8b005704b5e1be20c948b19bd6a
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-03-07 21:20:43 +0100

    setup server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+const port = process.env.PORT ? process.env.PORT : 3000
+app.use(express.static('public'))
+
+
+
+
+const server = app.listen(port, error => {
+    if (error) {
+        console.log(error)
+    }
+    console.log("Server is running on port", server.address().port)
+})
